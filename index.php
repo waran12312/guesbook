a7b688603ce057d86360c2ff6884002a2aa23905
waran <heloman.gamer@gmail.com>
2023-01-08 21:06:57 -0800

    fist commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="show_php">
+        what is your name:<input type="text" name="name"><br>
+        what is your age:<input type="text" name="age"><br>
+        <input type="Submit" value="Submit">
+    </form>
+</body>
+
+</html>
---
+++ show.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+
+    echo "Name:" .$name."<br>";
+    echo "Age:" .$age."<br>";  
+    
+    ?>
+</body>
+
+</html>
