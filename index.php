cae95878782845088d581395b4e4eb92fe372bae
waran12312 <120999715+waran12312@users.noreply.github.com>
2023-01-15 00:09:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <form action="show.php">
-        what is your name:<input type="text" name="name"><br>
-        what is your age:<input type="text" name="age"><br>
-        <input type="Submit" value="Submit">
-    </form>
-</body>
+<table align="center" style="margin-top: 340px;">
+<tr>
+<td>
+<form action="show.php"> 
 
+<select name="prefix">
+        <option value="">-เลือกเพศ-</option>
+        <option value="เด็กชาย">-เด็กชาย-</option>
+        <option value="เด็กหญิง">-เด็กหญิง-</option>
+        <option value="นาย">-นาย-</option>
+        <option value="นาง">-นาง-</option>
+        <option value="นางสาว">-นางสาว-</option> 
+            
+    </select>
+    <br>
+    
+    What is Your Name:   <input type="text"name="name"><br>
+    What is Your Age:    <input type="text"name="age"><br>
+    What is Your Sex:    <select type="text"name="sex"><br>
+        <option value="ชาย">-ชาย-</option>
+        <option value="หญิง">-หญิง-</option>
+        <option value="อื่นๆ">-อื่นๆ-</option>
+        </select>
+    <br>
+    other<input type="text"name="sex2">
+    <br>
+    What is Your Status: <input type="text"name="status"><br>
+    <input type="submit" value="submit"> 
+    
+</form>
+
+</td>
+</tr>
+</table>
+</body>
 </html>
