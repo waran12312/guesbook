092491a868494405e2c5575be9840d2bbc9f8350
waran12312 <120999715+waran12312@users.noreply.github.com>
2023-01-15 00:09:35 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
-    
 <body>
-    <?php
+<table align="center" style="margin-top: 340px;">
+<tr>
+<td>
+<?php 
+    $prefix = $_GET["prefix"];
     $name = $_GET["name"];
     $age = $_GET["age"];
-
-    echo "Name:" .$name."<br>";
-    echo "Age:" .$age."<br>";  
+    $sex = $_GET["sex"];
+    $status = $_GET["status"];
+    $sex2 = $_GET["sex2"];
+ 
     
-    ?>
-</body>
+    echo "Name:".$prefix.$name."<br>";
+    echo "Age:".$age."<br>";
+    echo "sex:".$sex."<br>";
+    echo "other:" .$sex2."<br>";
+    echo "status:".$status."<br>";
+    
 
+?>
+</td>
+</tr>
+</table>
+</body>
 </html>
